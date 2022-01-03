be544d0fcc98175844e2719e9a2add77af365344
FiveSi <95166661+FiveSi@users.noreply.github.com>
2022-01-03 16:15:17 +0800

    Add files via upload

---
+++ abcd.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const data = "b";
+// const number = 1024 * 1024 * 1024;
+const number = 1024 * 1024 * 500;
+console.log(number);
+console.log(268435455);
+// const data2 = data.repeat(268435455);
+// fs.writeFileSync("./b.txt", data2);
